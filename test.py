88d663644ec4e1f3737ef89f877cffdcb34ea4ea
shixuanLin1009 <james920402@gmail.com>
2025-09-02 09:47:52 +0800

    add some test

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-print("hello")
+import unittest
+
+# 被測試的程式
+def add(x: int, y: int) -> int:
+    return x + y
+
+def div(x: int, y: int) -> float:
+    return x / y
+
+# 測試類別要繼承 unittest.TestCase
+class TestMath(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+
+    def test_div(self):
+        self.assertAlmostEqual(div(10, 2), 5.0)
+        # 測試是否丟出例外
+        with self.assertRaises(ZeroDivisionError):
+            div(1, 0)
+
+# 直接執行測試
+if __name__ == '__main__':
+    unittest.main()
---
+++ tkiner_test.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+win=tk.Tk()
+win.title("Python GUI")
+
+
+win.geometry("600x300")
+win.minsize(600,300)
+win.maxsize(1000,300)
+win.config(bg="blue")
+win.mainloop()
